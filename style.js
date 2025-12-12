9978ccdf2d561cf74f417eea549962afcf5ff3ca
Prototypestore <dikeledireeks@outlook.com>
2025-12-12 16:12:07 +0200

    Update style.js

--- style.js
+++ style.js
@@ -3,7 +3,7 @@ const slides = Array.from(track.children);
 let index = 0;
 
 function showSlide(i) {
-  track.style.transform = `translateX(-${i * 450}px)`;
+  track.style.transform = `translateX(-${i * 480}px)`;
 }
 
 document.querySelector('.slide-button2').addEventListener('click', (e) => {
